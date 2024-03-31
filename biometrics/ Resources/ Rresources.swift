ac93095d5f7a809a76ec0e8a23fa9d6c172ee16c
Philippe MICHEL <philippe.michel1@gmail.com>
2024-03-31 19:55:06 +0200

    Gestion face ID

---
+++ biometrics/ Resources/ Rresources.swift
@@ -0,0 +1,8 @@
+//
+//   Rresources.swift
+//  biometrics
+//
+//  Created by Philippe MICHEL on 31/03/2024.
+//
+
+import Foundation
---
+++ biometrics/View/ViewPicture.swift
@@ -0,0 +1,18 @@
+//
+//  ViewPicture.swift
+//  biometrics
+//
+//  Created by Philippe MICHEL on 31/03/2024.
+//
+
+import SwiftUI
+
+struct ViewPicture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewPicture()
+}
