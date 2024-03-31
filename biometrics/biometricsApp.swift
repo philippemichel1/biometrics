31ba1c842df41288ad5b225f919f7c1ad439e123
Philippe MICHEL <philippe.michel1@gmail.com>
2024-03-31 16:28:32 +0200

    Initial Commit

---
+++ biometrics/biometricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometricsApp.swift
+//  biometrics
+//
+//  Created by Philippe MICHEL on 31/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct biometricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
