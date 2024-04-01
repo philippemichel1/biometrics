efa6e6cf41e346da35108b76a3cfb00f6a83e6da
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-01 13:23:13 +0200

    gestion des evenements  du téléphone

--- biometrics/View/ContentView.swift
+++ biometrics/View/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 import LocalAuthentication
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
     @State private var  isPresentedPicture = false
+    
     var body: some View {
         VStack {
             Text("Déverrouilez votre Iphone")
@@ -20,13 +22,29 @@ struct ContentView: View {
             Spacer()
         }
         .onAppear() {
-            authenticationBiometrics()
+            // demande l'autentification la photo est caché
+            if isPresentedPicture == false {
+                authenticationBiometrics()
+            }
         }
-        
+        // application active // arriere plan // inactive
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .active{
+                if isPresentedPicture == false {
+                    authenticationBiometrics()
+                }
+                
+            } else if newPhase == .background {
+                isPresentedPicture = false
+                
+            } else if newPhase == .inactive {
+                isPresentedPicture = false
+            }
+        }
     }
     
     // doc apple qui explique la mise en oeuvre
-// https://developer.apple.com/documentation/localauthentication/
+    // https://developer.apple.com/documentation/localauthentication/
     // fonction qui gere la biométrie
     func authenticationBiometrics() {
         // Un mécanisme d'évaluation des politiques d'authentification et des contrôles d'accès.
